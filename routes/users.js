90593e19bea424cee27ed470c7ea334a068190c3
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-01 18:13:50 +0530

    Check of authorization middleware

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const { authorize } = require("../middleware/authorization");
 const { contactUs, getUserById, deleteUserAndAssociatedLinks } = require("../controllers/userController");
 
 // Fetching all record of a user based on their Id
-userRouter.get("/:id", authorize, getUserById);
+userRouter.get("/:id",  getUserById);
 
 // Route for deleting a user and associated links
 userRouter.delete("/:id", authorize, deleteUserAndAssociatedLinks);
