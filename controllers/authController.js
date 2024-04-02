4db49889b49c69c55462cfe2a6dbf91139785889
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-02 14:36:39 +0530

    Change authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,7 +66,7 @@ const loginUser = async (req, res) => {
     const cookieOptions = {
       httpOnly: true,
       secure: true, 
-      domain: "localhost",
+      // domain: "localhost",
       sameSite: 'None', // Set SameSite to None
       path: "/",
     };
