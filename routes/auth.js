b635a41eac0a2ae18c57b9bdeab38e4b290314fd
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-02 12:40:51 +0530

    Change auth.js and authController.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 // Acquiring functions from auth controller
-const { registerUser, loginUser } = require("../controllers/authController");
+const { registerUser, loginUser, logoutUser } = require("../controllers/authController");
+const { authorize } = require("../middleware/authorization");
 
 // Creating a new user
 router.post("/register", registerUser);
 
 // Route for user login
 router.post("/login", loginUser);
+
+// Route for logging out a user
+userRouter.get("/logout", authorize, logoutUser);
 
 module.exports = router;
