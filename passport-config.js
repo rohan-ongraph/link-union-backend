cf40ea624927073fbb48921100f1e9006938a77d
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-04 17:22:23 +0530

    Change app.js

--- passport-config.js
+++ passport-config.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID: '532503575871-3naamp9l7ms1p6qeue6n612ql8n977lf.apps.googleusercontent.com',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://link-union-backend.onrender.com/auth/google/callback"
+      callbackURL: "https://link-union-backend.onrender.com/auth/google/callback"
   },
   googleStrategy
 ));
