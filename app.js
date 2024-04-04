56bf8df8ccbec9af17b7c54a88263ea896691401
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-04 17:24:45 +0530

    Change app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ setConnection(url);
 app.use(passport.initialize());
 
 //middlewares
-app.use(cors());
+app.use(cors({ origin: ['https://link-union.netlify.app', 'https://accounts.google.com'], credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
