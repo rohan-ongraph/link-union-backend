96f2b54a8db7932d772c99c65ea8fd8a8af0116b
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-04 15:38:49 +0530

    Change app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ setConnection(url);
 app.use(passport.initialize());
 
 //middlewares
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
