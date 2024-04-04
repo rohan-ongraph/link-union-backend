f92e9bf77bbde803b6934266c7c5d1b84f777213
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-04 15:34:51 +0530

    Add google auth

--- passport-config.js
+++ passport-config.js
@@ -3,7 +3,7 @@ const { googleStrategy } = require('./controllers/authController');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientID: '532503575871-3naamp9l7ms1p6qeue6n612ql8n977lf.apps.googleusercontent.com',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "https://link-union-backend.onrender.com/auth/google/callback"
   },
