f8ca9e12af78e6341621926429612f6ce5843bb6
Rohan Kandpal <rohan.ongraph@gmail.com>
2024-04-05 13:02:02 +0530

    Change authorization.js

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -4,12 +4,12 @@ const authorize = (req, res, next) => {
   try {
     const authorizationHeader = req.headers['x-auth-token'];
     if (!authorizationHeader) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: 'Unauthorized - no Header' });
     }
 
     const token = authorizationHeader.split(' ')[1];
     if (!token) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: 'Unauthorized - no Token' });
     }
 
     jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, user) => {
